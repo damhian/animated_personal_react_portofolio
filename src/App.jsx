4b35f76cc8c1e0afc741c2adfa47bc1337b4dfc9
damhian <david.mayshiogie@protonmail.com>
2025-11-11 14:32:04 +0700

    add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Navbar } from "./components/Navbar";
 import { Hero } from "./components/Hero";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import emailjs from "@emailjs/browser";
-import { motion, stagger } from "framer-motion";
+import { motion } from "framer-motion";
+
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -25,6 +25,7 @@ function App() {
       <Projects />
       <Contact />
 
+      <Analytics />
       <motion.footer
         className="footer"
         initial={{ opacity: 0 }}
